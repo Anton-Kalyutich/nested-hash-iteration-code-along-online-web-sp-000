80b908473297957461bbbf318ab983a5b0b4d81e
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-25 23:06:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |name, data|
     if name == "Freddy Mercury"
-      data/each do |attribute, value|
+      data.each do |attribute, value|
         if attribute == :favorite_ice_cream_flavors
           value.delete("strawberry")
       
